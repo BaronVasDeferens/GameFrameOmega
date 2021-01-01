1cc00a4a35c527397a582ec52ba85fc2c30104d8
Skot <baronvasdeferens@gmail.com>
2021-01-01 00:49:23 -0800

    Super crude

--- src/main/kotlin/GameFrame.kt
+++ src/main/kotlin/GameFrame.kt
@@ -1,22 +1,33 @@
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import java.awt.Canvas
 import java.awt.Dimension
 import java.awt.Graphics2D
 import java.awt.GraphicsEnvironment
 import java.awt.event.KeyListener
 import java.awt.image.BufferedImage
+import java.nio.Buffer
+import java.util.concurrent.Executors
 import javax.swing.JFrame
 import javax.swing.event.MouseInputAdapter
+import kotlin.coroutines.CoroutineContext
 
 
 class GameFrame(
     private val frameTitle: String = "TITLE OF GAME",
     private val width: Int = 1000,
-    private val height: Int = 1000) {
+    private val height: Int = 1000,
+    private val imageState: MutableStateFlow<BufferedImage>
+) {
 
     private val frame = JFrame()
     private val canvas = Canvas() // TODO: investigate graphicsConfiguration / DoubleBuffer?
 
-    private var backgroundImage : BufferedImage? = null
+    private var backgroundImage: BufferedImage? = null
+
 
     init {
 
@@ -36,6 +47,12 @@ class GameFrame(
         frame.pack()
 
         canvas.requestFocus()
+
+        imageState.onEach { image ->
+            drawImage(image)
+        }.launchIn(GlobalScope)
+
+
     }
 
     fun setKeyListener(listener: KeyListener) {
@@ -54,7 +71,7 @@ class GameFrame(
     fun drawImage(image: BufferedImage, x: Int = 0, y: Int = 0) {
         val graphics = canvas.graphics as Graphics2D
         backgroundImage?.let {
-            graphics.drawImage(backgroundImage, 0,0, null)
+            graphics.drawImage(backgroundImage, 0, 0, null)
         }
 
         graphics.drawImage(image, x, y, null)
--- src/main/kotlin/MainBrain.kt
+++ src/main/kotlin/MainBrain.kt
@@ -1,7 +1,7 @@
+import kotlinx.coroutines.flow.MutableStateFlow
 import java.awt.Color
 import java.awt.Graphics2D
 import java.awt.image.BufferedImage
-import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicInteger
 import java.util.concurrent.atomic.AtomicLong
 import kotlin.concurrent.thread
@@ -10,44 +10,59 @@ object MainBrain {
 
     @JvmStatic
     fun main(args: Array<String>) {
+        val width = 500
+        val height = 500
 
-        SimpleRender()
+
+        SimpleRender(width, height)
 
     }
 
-    class SimpleRender {
+    class SimpleRender(
+        private val width: Int = 500,
+        private val height: Int = 500,
+    ) {
+
+        val imageState = MutableStateFlow(BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB))
 
         val targetFps = 60
-
-        private val width = 500
-        private val height = 500
-
-        private val gameFrame = GameFrame("Let's try and animations! 2021", width, height)
+        private val gameFrame = GameFrame("Let's try and animations! 2021", width, height, imageState)
 
 
         init {
 
             thread {
+                val now = AtomicLong(System.currentTimeMillis())
                 val startTime = System.currentTimeMillis()
+                val elapsedTime = AtomicLong(0)
                 val lastRenderTime = AtomicLong(System.currentTimeMillis())
                 val totalRenders = AtomicInteger(0)
 
+                // TODO: adjust this value based on performance data
+                var waitBetweenRenders = 16L // Crude approx of 1000/60 = 16.667
+                var currentFps = 0.0f
+
                 while (true) {
 
-                    val elapsedTime = (System.currentTimeMillis() - startTime)
+                    now.set(System.currentTimeMillis())
+
+                    elapsedTime.set(System.currentTimeMillis() - startTime)
                     totalRenders.incrementAndGet()
+                    currentFps = totalRenders.get().toFloat() / (elapsedTime.get() / 1000).toFloat()
+
 
                     val image = BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
                     val g = image.graphics as Graphics2D
                     g.color = Color.BLACK
                     g.fillRect(0, 0, width, height)
                     g.color = Color.RED
-                    g.drawString("ELAPSED: ${elapsedTime/1000} seconds", 50f, 50f)
+                    g.drawString("ELAPSED: ${elapsedTime.get() / 1000} seconds", 50f, 50f)
                     g.drawString("TOTAL FRAMES: ${totalRenders.get()}", 50f, 75f)
-                    g.drawString("FPS: ${totalRenders.get().toFloat() / (elapsedTime / 1000).toFloat()}", 50f, 100f)
-                    gameFrame.drawImage(image)
-
+                    g.drawString("FPS: ${currentFps}", 50f, 100f)
                     g.dispose()
+                    lastRenderTime.set(System.currentTimeMillis())
+                    imageState.value = image
+                    Thread.sleep(waitBetweenRenders)
                 }
             }
 
