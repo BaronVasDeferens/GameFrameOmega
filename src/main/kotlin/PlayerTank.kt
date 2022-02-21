0127158b469ea5cd240b2e4ccd27f55dce47ca30
Skot <baronvasdeferens@gmail.com>
2022-02-20 16:50:42 -0800

    included a coasting to stop mechanic

--- src/main/kotlin/PlayerTank.kt
+++ src/main/kotlin/PlayerTank.kt
@@ -1,23 +1,25 @@
+import KeyboardInputAdapter.KeyState.MOVE_UP
 import java.awt.Graphics2D
 import java.awt.image.BufferedImage
 import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicInteger
 import javax.imageio.ImageIO
 
-abstract class Player(open var x: Int,
-                      open var y: Int,
-                      open val movementPerUpdate: Int,
-                      val spriteSize: Int = 64
-                      ) {
+abstract class Player(
+    open var x: Int,
+    open var y: Int,
+    open val movementPerUpdate: Int,
+    val spriteSize: Int = 64
+) {
     val isMoving = AtomicBoolean(false)
 }
 
-class PlayerTank (
+class PlayerTank(
     override var x: Int,
     override var y: Int,
-    override val movementPerUpdate: Int = 2,
+    override val movementPerUpdate: Int = 4,
     spriteFileName: String = "tank_sprite_sheet.png"
-): Player(x,y, movementPerUpdate) , Renderable {
+) : Player(x, y, movementPerUpdate), Renderable {
     private var spriteSheet: BufferedImage = ImageIO.read(javaClass.classLoader.getResourceAsStream(spriteFileName))
     private val frameColumn = AtomicInteger(0)
     private val frameRow = AtomicInteger(0)
@@ -28,15 +30,22 @@ class PlayerTank (
     private val currentTicks = AtomicInteger(0)
 
 
+    private var lastDirection = MOVE_UP
+    private val coastingDeltaMax = 10
+    private var coastingDeltaCurrent = 0
+
     fun move(directions: Set<KeyboardInputAdapter.KeyState>) {
 
         isMoving.set(directions.isNotEmpty())
 
         if (isMoving.get()) {
 
+            lastDirection = directions.first()
+            coastingDeltaCurrent = 1
+
             when (directions.first()) {
 
-                KeyboardInputAdapter.KeyState.MOVE_UP -> {
+                MOVE_UP -> {
                     y -= movementPerUpdate
                     frameRow.set(0)
                 }
@@ -60,11 +69,16 @@ class PlayerTank (
 
                 }
             }
+        } else {
+
         }
     }
 
     fun update() {
         if (isMoving.get()) {
+
+            coastingDeltaCurrent = 1
+
             if (currentTicks.incrementAndGet() >= ticksPerFrame) {
                 frameColumn.set(frameColumn.incrementAndGet() % maxColumns)
                 currentTicks.set(0)
@@ -73,16 +87,70 @@ class PlayerTank (
             }
         } else {
 
+            if (coastingDeltaCurrent > 0) {
+                coastingDeltaCurrent++
+            }
+
+            if (coastingDeltaCurrent >= coastingDeltaMax) {
+                coastingDeltaCurrent = 0
+            }
+
+
+            // Apply drift (if any)
+            if (coastingDeltaCurrent > 0) {
+
+                if (currentTicks.incrementAndGet() >= ticksPerFrame) {
+                    frameColumn.set(frameColumn.incrementAndGet() % maxColumns)
+                    currentTicks.set(0)
+                }
+
+
+                val driftAmount = (coastingDeltaMax / coastingDeltaCurrent)
+
+                println("dir: $lastDirection delta: $coastingDeltaCurrent amt: $driftAmount")
+
+                when (lastDirection) {
+
+                    MOVE_UP -> {
+                        y -= driftAmount
+                        frameRow.set(0)
+                    }
+
+                    KeyboardInputAdapter.KeyState.MOVE_RIGHT -> {
+                        x += driftAmount
+                        frameRow.set(1)
+                    }
+
+                    KeyboardInputAdapter.KeyState.MOVE_DOWN -> {
+                        y += driftAmount
+                        frameRow.set(2)
+                    }
+
+                    KeyboardInputAdapter.KeyState.MOVE_LEFT -> {
+                        x -= driftAmount
+                        frameRow.set(3)
+                    }
+
+                    else -> {
+
+                    }
+                }
+            }
+
+
             // put mouse in "neutral" frame
 //            frameRow.set(2)
 //            frameColumn.set(1)
         }
 
+
+
 //        println("row: ${frameRow.get()} col: ${frameColumn.get()}")
     }
 
     override fun render(graphics2D: Graphics2D) {
-        val targetFrame = spriteSheet.getSubimage(frameColumn.get() * frameSize, frameRow.get() * frameSize, frameSize, frameSize)
+        val targetFrame =
+            spriteSheet.getSubimage(frameColumn.get() * frameSize, frameRow.get() * frameSize, frameSize, frameSize)
         graphics2D.drawImage(targetFrame, x, y, null)
     }
 
