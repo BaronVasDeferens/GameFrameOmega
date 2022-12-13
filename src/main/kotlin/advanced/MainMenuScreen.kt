ce38227f0d7ca612215684c85fd4f08a0bdc8d2f
West <swest@leanpath.com>
2022-12-13 08:33:08 -0800

    Intro screen tweaks

--- src/main/kotlin/advanced/MainMenuScreen.kt
+++ src/main/kotlin/advanced/MainMenuScreen.kt
@@ -1,6 +1,7 @@
 package advanced
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.utils.ScreenUtils
@@ -14,17 +15,17 @@ class MainMenuScreen(private val drop: Drop):Screen {
     }
 
     override fun render(delta: Float) {
-        ScreenUtils.clear(.9f, 0f, 0.2f, 1f);
+        ScreenUtils.clear(0.0f, 0.0f, 0.0f, 1f);
 
         camera.update();
         drop.batch.projectionMatrix = camera.combined;
 
         drop.batch.begin();
-        drop.font.draw(drop.batch, "Welcome to advanced.Drop!!! ", 100f, 150f)
-        drop.font.draw(drop.batch, "Tap anywhere to begin!", 100f, 100f)
+        drop.font.draw(drop.batch, "W A N D E R E R", 100f, 150f)
+        drop.font.draw(drop.batch, "press SPACE to begin", 100f, 100f)
         drop.batch.end()
 
-        if (Gdx.input.isTouched) {
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
             drop.screen = MazeScreen(drop)
             dispose()
         }
