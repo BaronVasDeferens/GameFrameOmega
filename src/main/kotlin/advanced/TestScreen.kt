f4ba82c958825a2e90a0e70713ef5fa13adabf2a
scottwestphotonmarine <scott.west@photonmarine.com>
2023-08-06 22:51:25 -0700

    Works-ish, not great

--- src/main/kotlin/advanced/TestScreen.kt
+++ src/main/kotlin/advanced/TestScreen.kt
@@ -10,37 +10,46 @@ import com.badlogic.gdx.graphics.Pixmap
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.TextureRegion
-import com.badlogic.gdx.scenes.scene2d.ui.TextTooltip
 import com.badlogic.gdx.utils.ScreenUtils
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
-import org.lwjgl.glfw.GLFW
-import java.util.concurrent.Executors
-import java.util.concurrent.ScheduledFuture
-import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 import kotlin.system.exitProcess
 
 class TestScreen(private val drop: Drop) : Screen {
 
+    val playerInputQueue = MutableStateFlow<Set<MovementDirection>>(setOf(MovementDirection.NOT_MOVING))
+
+
     private val camera = OrthographicCamera()
-    private val keyboardInputProcessor = KeyboardInputProcessor()
+    private val keyboardInputProcessor = KeyboardInputProcessor(playerInputQueue)
     private val frameCounter = FrameCounter()
     private val renderer = Renderer(drop.width, drop.height)
 
-    private val spriteSheet = Spritesheet("robot_basic_sprite_sheet.png", 1, 4, 64, 64)
+    private val animationDirectives = MutableStateFlow<List<MovementDirective>>(listOf())
+
+    private val playerEntity = GameEntity(type = GameEntityType.PLAYER)
 
 
     init {
         Gdx.input.inputProcessor = keyboardInputProcessor
         camera.setToOrtho(false, drop.width.toFloat(), drop.height.toFloat())
 
-//        frameCounter.frameCountFlow.onEach {
-//            println(it)
-//        }.launchIn(GlobalScope)
+
+        playerInputQueue.onEach { inputs ->
+            inputs.map { input ->
+                animationDirectives.value = animationDirectives.value.plus(
+                    MovementDirective(playerEntity, input, 1, 20)
+                )
+            }
+        }.launchIn(GlobalScope)
+
+        frameCounter.frameCountFlow.onEach { count ->
+            animationDirectives.value = animationDirectives.value.filter { it.isAlive() }
+        }.launchIn(GlobalScope)
+
 
     }
 
@@ -52,7 +61,19 @@ class TestScreen(private val drop: Drop) : Screen {
         drop.batch.begin()
 
         renderer.render().draw(drop.batch)
-        drop.batch.draw(spriteSheet.getTextureAtIndex(frameCounter.frameCountFlow.value), 0.0f, 0.0f)
+
+        animationDirectives.value.forEach { directive ->
+            directive.update()
+            val texture = directive.getTexture()
+            val coordinates = directive.getCoordinates()
+
+            drop.batch.draw(
+                texture,
+                coordinates.first.toFloat(),
+                coordinates.second.toFloat()
+            )
+        }
+
         drop.batch.end()
     }
 
@@ -124,6 +145,29 @@ class Renderer(val width: Int, val height: Int) {
     }
 }
 
+enum class GameEntityType {
+    PLAYER,
+    ENEMY
+}
+
+class GameEntity(val type: GameEntityType, var x: Int = 0, var y: Int = 0) {
+
+    private val spriteSheet = when (type) {
+        GameEntityType.PLAYER -> {
+            Spritesheet("robot_basic_sprite_sheet.png", 1, 4, 64, 64)
+        }
+
+        else -> {
+            Spritesheet("robot_basic_sprite_sheet.png", 1, 4, 64, 64)
+        }
+    }
+
+    fun getTextureAtIndex(index: Int): TextureRegion {
+        return spriteSheet.getTextureAtIndex(index)
+    }
+
+}
+
 class Spritesheet(fileName: String, rows: Int, columns: Int, cellWidth: Int, cellHeight: Int) {
 
     private val textureRegions = mutableListOf<TextureRegion>()
@@ -146,16 +190,62 @@ class Spritesheet(fileName: String, rows: Int, columns: Int, cellWidth: Int, cel
 }
 
 
-class KeyboardInputProcessor() : InputProcessor {
+enum class MovementDirection(val deltaX: Int, val deltaY: Int) {
+    NOT_MOVING(0, 0),
+    MOVING_LEFT(-1, 0),
+    MOVING_RIGHT(1, 0),
+    MOVING_UP(0, 1),
+    MOVING_DOWN(0, -1)
+}
+
+data class MovementDirective(
+    val entity: GameEntity,
+    val direction: MovementDirection,
+    val pixelsPerTick: Int,
+    val ticksMax: Int
+) {
+
+    private var tickCurrent: Int = 0
+    private var isActive = true
+
+    fun update() {
+        tickCurrent++
+        if (!isAlive()) {
+            if (isActive) {
+                entity.x = entity.x + (ticksMax * pixelsPerTick * direction.deltaX)
+                entity.y = entity.y + (ticksMax * pixelsPerTick * direction.deltaY)
+            }
+            isActive = false
+        }
+    }
+
+    fun getTexture(): TextureRegion {
+        return entity.getTextureAtIndex(0)
+    }
+
+    fun getCoordinates(): Pair<Int, Int> {
+        return Pair(
+            (entity.x + ((tickCurrent * pixelsPerTick) * direction.deltaX)),
+            ((entity.y + (tickCurrent * pixelsPerTick)) * direction.deltaY)
+        )
+    }
+
+    fun isAlive(): Boolean {
+        return tickCurrent < ticksMax
+    }
+
+}
+
+class KeyboardInputProcessor(val playerInputQueue: MutableStateFlow<Set<MovementDirection>>) : InputProcessor {
     override fun keyDown(keycode: Int): Boolean {
         when (keycode) {
 
             Input.Keys.A -> {
-
+                playerInputQueue.value = playerInputQueue.value.plus(MovementDirection.MOVING_LEFT)
             }
 
             Input.Keys.D -> {
-
+                playerInputQueue.value = playerInputQueue.value.plus(MovementDirection.MOVING_RIGHT)
             }
 
             Input.Keys.ESCAPE -> {
@@ -166,7 +256,22 @@ class KeyboardInputProcessor() : InputProcessor {
     }
 
     override fun keyUp(keycode: Int): Boolean {
-        return false
+        when (keycode) {
+
+            Input.Keys.A -> {
+                playerInputQueue.value = playerInputQueue.value.minus(MovementDirection.MOVING_LEFT)
+            }
+
+            Input.Keys.D -> {
+                playerInputQueue.value = playerInputQueue.value.minus(MovementDirection.MOVING_RIGHT)
+            }
+
+            Input.Keys.ESCAPE -> {
+                exitProcess(0)
+            }
+        }
+
+        return true
     }
 
     override fun keyTyped(character: Char): Boolean {
